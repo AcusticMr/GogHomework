735de220ce0c5630a15cc6ed0366c305f67ccb68
Marcin Koszewski <marcin.koszewski@protonmail.com>
2020-12-20 19:12:06 +0100

    add discountBadge

---
+++ src/components/DiscountBadge.vue
@@ -0,0 +1,29 @@
+<template>
+  <span class="discount-badge">
+    - {{ discountPercentage }}%
+  </span>
+</template>
+
+<script>
+export default {
+  name: "DiscountBadge",
+  props: {
+    discountPercentage: {
+      type: Number,
+      required: true
+    },
+  }
+}
+</script>
+
+<style lang="sass">
+.discount-badge
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 64px
+  border-radius: 4px
+  margin-right: 8px
+  color: white
+  background-color: seagreen
+</style>
