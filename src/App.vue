7917f24bfbbf3f39ee2077a18255dff3ba055648
Marcin Koszewski <marcin.koszewski@protonmail.com>
2020-12-20 14:35:36 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div>NavBar placeholder</div>
+    <div>Content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+}
+</style>
