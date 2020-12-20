b42a0d1d6bcd7a5e6455f272a5daa8609513dc28
Marcin Koszewski <marcin.koszewski@protonmail.com>
2020-12-20 19:10:39 +0100

    add/use banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="banner">
+    <div class="banner__title">
+      <h3>{{ title }}</h3>
+    </div>
+
+    <div @click="redirect" class="banner__img-wrapper">
+      <img class="banner__img" :src="imgUrl" alt="banner-img">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  props: {
+    title: {
+      type: String,
+      default: 'GAME OF THE WEEK'
+    },
+    imgUrl: {
+      type: String,
+      required: true
+    },
+    redirectUrl: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    // banner redirect to item page
+    redirect() {
+      window.open(this.redirectUrl,"_self");
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.banner
+  display: flex
+  margin-top: 32px
+  padding-bottom: 16px
+  flex-direction: column
+  height: 600px
+  width: 100%
+
+  &__title
+    margin: 0 0 32px
+
+    h3
+      margin: 0
+
+  &__img-wrapper
+    position: relative
+    height: 100%
+    width: 100%
+    overflow: hidden
+    cursor: pointer
+
+  &__img
+    width: 100%
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <div class="home__banner">
-      banner game of week
+      <banner
+        img-url="https://images.gog-statics.com/5bc58089e14771de0c1b872c529c8b8b150b87fe28c7e4db1856389526849bb3.jpg"
+        redirect-url="/game/witcher_adventure_game"
+      />
     </div>
     <div class="home__products">
       products card
@@ -10,9 +13,13 @@
 </template>
 
 <script>
+import Banner from "@/components/Banner";
 
 export default {
   name: 'Home',
+  components: {
+    Banner,
+  },
 }
 </script>
 
