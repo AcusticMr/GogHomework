934b41c66a52120b817470f897c4322c6ae4d644
Marcin Koszewski <marcin.koszewski@protonmail.com>
2020-12-22 14:24:03 +0100

    add icon component lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store/index';
+import router from './router';
+import feather from 'vue-icon';
 
 Vue.config.productionTip = false
+
+Vue.use(feather, 'v-icon');
 
 // custom filters
 import './filters/index';
