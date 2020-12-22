c0da18b391203685b59519a11bb78e6fe3963f53
Marcin Koszewski <marcin.koszewski@protonmail.com>
2020-12-22 14:23:49 +0100

    add vuex module store

---
+++ src/store/modules/cart.store.js
@@ -0,0 +1,23 @@
+// State object
+const state = {
+}
+
+// Getter functions
+const getters = {
+}
+
+// Mutations
+const mutations = {
+}
+
+// Actions
+const actions = {
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
